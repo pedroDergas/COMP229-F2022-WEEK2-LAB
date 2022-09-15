3e21b1b9313d8777fb75ecd6e0f723448a894fd2
pedroDergas <pedrodergado@gmail.com>
2022-09-14 23:25:47 -0400

    hey

--- lab2.js
+++ lab2.js
@@ -1,4 +1,4 @@
-const http = require('http');
+/*const http = require('http');
 const url = require('url');
 
 http.createServer((req, res) => {
@@ -6,21 +6,52 @@ http.createServer((req, res) => {
 
     Logger(); // this is my first middleware
 
-    if (queryObject['a'] === 'a') {
+
+    if (queryObject['a'] === 'html') {
         res.writeHead(200, {
             'Content-Type': 'text/plain'
         });
-        res.end('Hello World');
+        res.end('Hello from NodeJS Application');
     } else {
         res.writeHead(200, {
             'Content-Type': 'text/plain'
         });
-        res.end('Bye World');
+        res.end('Hello from NodeJS Application as json');
+        console.log(queryObject);
     }
 
 
 }).listen(3000);
 
-function Looger() {
+function Logger() {
     console.log('Received a request');
-}
+}*/
+
+const connect = require('connect');
+const app = connect();
+
+function threeThousand(req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello from NodeJS Application');
+};
+
+function hellohtml(req, res, next) {
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1> Hello from NodeJS Application as html</h1>');
+};
+
+function goodbyeWorld(req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
+    res.json({
+        message: "Hello from NodeJS Application as json"
+    }
+    );
+};
+
+app.use('/html', hellohtml);
+app.use('/json', goodbyeWorld);
+app.use('/', threeThousand);
+
+
+
+app.listen(3000);
